814654ed98be51ca9f24d6c01f6865dcd3a7c4d5
Miguel Garoz <mgaroz@gmail.com>
2023-04-04 02:10:06 -0600

    perf(app): more lazy loading images

--- src/routes/Brands.svelte
+++ src/routes/Brands.svelte
@@ -8,13 +8,13 @@
 
 <section
 	id="brands"
-	class="bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-blue-violet-500 via-boston-blue-500 to-[#52758e] pb-28"
+	class="from-blue-violet-500 via-boston-blue-500 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] to-[#52758e] pb-28"
 >
-	<div class="h-48 bg-white/10 backdrop-blur-sm flex justify-between px-16 items-center">
-		<img src={logo1} alt="logo1" class="w-1/6" />
-		<img src={logo2} alt="logo2" class="w-1/6" />
-		<img src={logo3} alt="logo3" class="w-1/6" />
-		<img src={logo4} alt="logo4" class="w-1/6" />
-		<img src={logo5} alt="logo5" class="w-1/6" />
+	<div class="flex h-48 items-center justify-between bg-white/10 px-16 backdrop-blur-sm">
+		<img loading="lazy" src={logo1} alt="logo1" class="w-1/6" />
+		<img loading="lazy" src={logo2} alt="logo2" class="w-1/6" />
+		<img loading="lazy" src={logo3} alt="logo3" class="w-1/6" />
+		<img loading="lazy" src={logo4} alt="logo4" class="w-1/6" />
+		<img loading="lazy" src={logo5} alt="logo5" class="w-1/6" />
 	</div>
 </section>
