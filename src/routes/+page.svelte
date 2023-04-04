3f33827685a5b6f0d4a698ed26946e9c5a9e44ea
Miguel Garoz <mgaroz@gmail.com>
2023-04-04 02:38:50 -0600

    refactor(app): working links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,33 +11,33 @@
 
 <section id="hero">
 	<div
-		class="min-h-screen flex items-start justify-between bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-blue-violet-500 via-boston-blue-500 to-[#52758e] px-14 pt-[88px] gap-20 pb-28"
+		class="from-blue-violet-500 via-boston-blue-500 flex min-h-screen items-start justify-between gap-20 bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] to-[#52758e] px-14 pb-28 pt-[88px]"
 	>
-		<div id="left" class="flex gap-16 flex-col w-1/6 min-h-screen">
+		<div id="left" class="flex min-h-screen w-1/6 flex-col gap-16">
 			<div id="logo" class="text-4xl font-bold">AiRobo</div>
 			<div id="nav">
-				<ul class="flex gap-6 flex-col text-[18px] text-white/70">
-					<li>Home</li>
-					<li>Platform</li>
-					<li>Company</li>
-					<li>Products</li>
-					<li>Resources</li>
+				<ul class="flex flex-col gap-6 text-[18px] text-white/70">
+					<li class="group"><a href="/" class="group-hover:underline">Home</a></li>
+					<li class="group"><a href="#showcase" class="group-hover:underline">Platform</a></li>
+					<li class="group"><a href="#develop" class="group-hover:underline">Company</a></li>
+					<li class="group"><a href="#results" class="group-hover:underline">Products</a></li>
+					<li class="group"><a href="#features" class="group-hover:underline">Resources</a></li>
 				</ul>
 			</div>
 			<div id="tech" class="flex flex-col gap-12">
 				<h2 class="text-2xl">Our Technologies</h2>
 				<div class="inline-block">
-					<span class="text-white/50 text-[14px]">01/</span>
+					<span class="text-[14px] text-white/50">01/</span>
 					<div class="inline-block w-[85%]">
-						<div class="border-b-2 border-white/60 flex justify-between items-center">
-							<p class="text-xl inline-block">Rillcuy</p>
+						<div class="flex items-center justify-between border-b-2 border-white/60">
+							<p class="inline-block text-xl">Rillcuy</p>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
 								stroke-width="2"
 								stroke="currentColor"
-								class="w-5 h-5 inline-block"
+								class="inline-block h-5 w-5"
 							>
 								<path
 									stroke-linecap="round"
@@ -49,17 +49,17 @@
 					</div>
 				</div>
 				<div class="inline-block">
-					<span class="text-white/50 text-[14px]">02/</span>
+					<span class="text-[14px] text-white/50">02/</span>
 					<div class="inline-block w-[85%]">
-						<div class="border-b-2 border-white/60 flex justify-between items-center">
-							<p class="text-xl inline-block">Mdituru</p>
+						<div class="flex items-center justify-between border-b-2 border-white/60">
+							<p class="inline-block text-xl">Mdituru</p>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
 								stroke-width="2"
 								stroke="currentColor"
-								class="w-5 h-5 inline-block"
+								class="inline-block h-5 w-5"
 							>
 								<path
 									stroke-linecap="round"
@@ -71,17 +71,17 @@
 					</div>
 				</div>
 				<div class="inline-block">
-					<span class="text-white/50 text-[14px]">03/</span>
+					<span class="text-[14px] text-white/50">03/</span>
 					<div class="inline-block w-[85%]">
-						<div class="border-b-2 border-white/60 flex justify-between items-center">
-							<p class="text-xl inline-block">Kockgmg</p>
+						<div class="flex items-center justify-between border-b-2 border-white/60">
+							<p class="inline-block text-xl">Kockgmg</p>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
 								viewBox="0 0 24 24"
 								stroke-width="2"
 								stroke="currentColor"
-								class="w-5 h-5 inline-block"
+								class="inline-block h-5 w-5"
 							>
 								<path
 									stroke-linecap="round"
@@ -93,9 +93,9 @@
 					</div>
 				</div>
 			</div>
-			<div id="footer" class="flex min-h-full justify-between items-end flex-1">
+			<div id="footer" class="flex min-h-full flex-1 items-end justify-between">
 				<!-- <div class="flex items-start justify-center h-full"> -->
-				<p class="-rotate-90 inline-block -translate-y-[150px]">Scroll down</p>
+				<p class="inline-block -translate-y-[150px] -rotate-90">Scroll down</p>
 				<!-- </div> -->
 				<div>
 					<p class="text-xl">Into AI <br /> better life <br /> services</p>
@@ -104,29 +104,29 @@
 		</div>
 		<div id="center" class="w-3/6">
 			<div class="w-full">
-				<h1 class="text-8xl uppercase font-sans font-bold z-10 relative inline-block left-[11%]">
+				<h1 class="relative left-[11%] z-10 inline-block font-sans text-8xl font-bold uppercase">
 					Let's <br /> improve <br /> your life
 				</h1>
 			</div>
 			<img src={heroImage} alt="robot" class="-mt-8" />
 		</div>
-		<div id="right" class="w-1/6 flex flex-col gap-16">
+		<div id="right" class="flex w-1/6 flex-col gap-16">
 			<button
-				class="uppercase rounded-full bg-transparent border px-12 py-5 mb-96 font-sans font-bold whitespace-nowrap hover:bg-white hover:text-boston-blue-800 transition-colors duration-200"
+				class="hover:text-boston-blue-800 mb-96 whitespace-nowrap rounded-full border bg-transparent px-12 py-5 font-sans font-bold uppercase transition-colors duration-200 hover:bg-white"
 				>Get Started</button
 			>
 			<div id="chips">
 				<div>
-					<p class="bg-white rounded-full text-black inline-block px-4 py-1">robotics</p>
-					<p class="bg-boston-blue-800 rounded-full inline-block px-4 py-1">automation</p>
+					<p class="inline-block rounded-full bg-white px-4 py-1 text-black">robotics</p>
+					<p class="bg-boston-blue-800 inline-block rounded-full px-4 py-1">automation</p>
 				</div>
 				<div>
-					<p class="bg-boston-blue-800 rounded-full inline-block px-4 py-1">technology</p>
-					<p class="bg-white rounded-full text-black inline-block px-4 py-1">2023</p>
+					<p class="bg-boston-blue-800 inline-block rounded-full px-4 py-1">technology</p>
+					<p class="inline-block rounded-full bg-white px-4 py-1 text-black">2023</p>
 				</div>
 				<div>
-					<p class="bg-white rounded-full text-black inline-block px-4 py-1">real time</p>
-					<p class="bg-boston-blue-800 rounded-full inline-block px-4 py-1">rebrand</p>
+					<p class="inline-block rounded-full bg-white px-4 py-1 text-black">real time</p>
+					<p class="bg-boston-blue-800 inline-block rounded-full px-4 py-1">rebrand</p>
 				</div>
 			</div>
 			<div id="description" class="flex items-end">
