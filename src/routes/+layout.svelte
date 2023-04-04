c956ae8076520017bd91e80df5a1ed0bd5ddcd48
Miguel Garoz <mgaroz@gmail.com>
2023-04-04 02:22:51 -0600

    added description and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,13 @@
 	import '../global.css';
 </script>
 
+<svelte:head>
+	<title>AiRobo</title>
+	<meta name="robots" content="index nofollow" />
+	<meta
+		name="description"
+		content="AiRobo is a company that specializes in improving quality of life through AI."
+	/>
+</svelte:head>
+
 <slot />
