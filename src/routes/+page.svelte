36a4c3dfcbb21ced28a14d5bc45a6e743af24bfa
Miguel Garoz <mgaroz@gmail.com>
2023-04-17 01:01:47 -0600

    changed wording

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,7 +52,7 @@
 					<span class="text-[14px] text-white/50">02/</span>
 					<div class="inline-block w-[85%]">
 						<div class="flex items-center justify-between border-b-2 border-white/60">
-							<p class="inline-block text-xl">Mdituru</p>
+							<p class="inline-block text-xl">Mditerm</p>
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								fill="none"
